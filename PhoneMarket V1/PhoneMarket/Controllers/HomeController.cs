27ec11dca60cfe619b02603df8d21305e7eb9b0c
ku zakhar <nik.name.doker@gmail.com>
2021-05-01 22:29:00 +0200

    MVC Phone store v1

---
+++ PhoneMarket V1/PhoneMarket/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PhoneMarket.Models;
+
+namespace PhoneMarket.Controllers
+{
+    public class HomeController : Controller
+    {
+        PhoneContext phoneContext = new PhoneContext();
+
+        public ActionResult Index()
+        {
+            IEnumerable<Phone> phones = phoneContext.Phones;
+
+            ViewBag.Phones = phones;
+
+            return View();
+        }
+
+
+        public ActionResult Purchases()
+        {
+            IEnumerable<Purchase> purchases = phoneContext.Purchases;
+
+            ViewBag.Purchase = purchases;
+
+            return View();
+        }
+
+
+
+
+
+        [HttpGet]
+        public ActionResult Buy(int id)
+        {
+            ViewBag.Id = id;
+
+            return View();
+        }
+
+        [HttpPost]
+        public string Buy(Purchase purchase)
+        {
+
+            purchase.DateTime = DateTime.Now;
+            phoneContext.Purchases.Add(purchase);
+            phoneContext.SaveChanges();
+            return $"Dear user, {purchase.FIO}, we will call soon";
+
+        }
+
+    }
+}
---
+++ PhoneMarket V1/PhoneMarket/Models/DBInitializer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+namespace PhoneMarket.Models
+{
+    public class DBInitializer :DropCreateDatabaseAlways<PhoneContext>
+    {
+
+
+        protected override void Seed(PhoneContext context)
+        {
+            context.Phones.Add(new Phone() { Name = "Huawei P20", Price = 40000, Producer = "Huawei" });
+            context.Phones.Add(new Phone() {  Name = "Z3 Play", Price = 34000, Producer = "Motorola" });
+            context.Phones.Add(new Phone() {  Name = "Mi Note 9", Price = 25000, Producer = "Xiaomi" });
+
+            base.Seed(context);
+        }
+    }
+
+
+}
